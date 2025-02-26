bb27b14239eda3019616a19c61db3417696a0733
Bilal-dev <143352356+bilalvakani@users.noreply.github.com>
2025-02-26 05:51:18 -0800

    make sidebar and navbar

---
+++ utils/navbar.jsx
@@ -0,0 +1,40 @@
+"use client";
+import React from "react";
+import { FaSearch, FaBell, FaBars } from "react-icons/fa";
+import { FiAlignLeft } from "react-icons/fi";
+
+const Navbar = ({ title, isSidebarOpen, setIsSidebarOpen }) => {
+  return (
+    <div className={`bg-white mt-4 flex justify-between items-center px-1 md:px-8 py-1 h-[57px] fixed top-0 left-0 right-0 transition-all duration-300 
+      ${isSidebarOpen ? "ml-[272px]" : "ml-24"}
+    `}>
+      {/* Left Section */}
+      <div className="flex items-center gap-2 md:gap-4">
+        {/* Toggle Sidebar */}
+        <FiAlignLeft className="text-black w-24 h-24 md:w-[21px] md:h-[24px] cursor-pointer" onClick={() => setIsSidebarOpen(!isSidebarOpen)} />
+        <h1 className="font-bold text-black text-lg md:text-2xl">{title}</h1>
+      </div>
+
+      {/* Right Section */}
+      <div className="flex items-center gap-4 md:gap-6">
+        <FaSearch className="text-gray-600 cursor-pointer w-5 h-5 md:w-6 md:h-6" />
+        <div className="relative">
+          <FaBell className="text-gray-600 cursor-pointer w-6 h-6 md:w-[27px] md:h-[29px]" />
+          <span className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full px-1">!</span>
+        </div>
+        <button className="bg-black text-white px-3 md:px-4 py-1 md:py-2 rounded-md text-xs md:text-sm">+ Add Story</button>
+        <div className="flex items-center gap-1 md:gap-2">
+          <div className="w-8 h-8 md:w-10 md:h-10 bg-gray-300 rounded-full flex items-center justify-center border border-gray-400">
+            <span className="text-xs">GK</span>
+          </div>
+          <div>
+            <p className="font-medium text-sm md:text-base">Gladys Kanyinda</p>
+            <p className="text-gray-600 text-xs bg-gray-500 px-2 rounded-md">Admin</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
