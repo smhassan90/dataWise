07d0511d751dec402a08b5b339ca2cacb4ac8c93
Bilal-dev <143352356+bilalvakani@users.noreply.github.com>
2025-03-20 11:03:07 -0700

    feat: implement settings form with profile image upload and password change functionality

--- src/utils/button.jsx
+++ src/utils/button.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+// import React from "react";
 
-export const Button = ({children,className,onClick }) => {
+// export const Button = ({children,className,onClick }) => {
+//   return (
+//     <button className={`bg-secondary items-center justify-center flex text-white rounded-large shadow-md px-normal py-2 text-sm font-medium transition-all duration-300 ${className}`}
+//     onClick={onClick}
+//     >
+//       {children}
+//     </button>
+//   );
+// };
+
+
+import React, { useState } from "react";
+
+export const Button = ({ children, className, onClick }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = (e) => {
+    setIsClicked(true);
+    if (onClick) {
+      onClick(e);
+    }
+
+    // 10 sec baad wapas default state par
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 1000); // 10000ms = 10 seconds
+  };
+
   return (
-    <button className={`bg-secondary items-center justify-center flex text-white rounded-large shadow-md px-normal py-2 text-sm font-medium transition-all duration-300 ${className}`}
-    onClick={onClick}
+    <button
+      className={`items-center justify-center flex rounded-large shadow-md px-normal py-2 text-sm font-medium transition-all duration-300 ${
+        isClicked ? "bg-white text-secondary" : "bg-secondary text-white"
+      } ${className}`}
+      onClick={handleClick}
     >
       {children}
     </button>
